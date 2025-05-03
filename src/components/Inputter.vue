0e6a4914f6d5628520edee7a016d2c7841ad2431
ambiesoft <ambiesoft.trueff@gmail.com>
2025-05-03 15:11:17 +0900

    refine

--- src/components/Inputter.vue
+++ src/components/Inputter.vue
@@ -9,77 +9,116 @@ defineProps({
 
 const input = ref();
 
-function getTusta(c) {
-  let ret = '';
-  switch (c) {
-    case '0': ret = '数字のゼロ'; break;
-    case '1': ret = '数字のイチ'; break;
-    case '2': ret = '数字のニ'; break;
-    case '3': ret = '数字のサン'; break;
-    case '4': ret = '数字のヨン'; break;
-    case '5': ret = '数字のゴ'; break;
-    case '6': ret = '数字のロク'; break;
-    case '7': ret = '数字のナナ'; break;
-    case '8': ret = '数字のハチ'; break;
-    case '9': ret = '数字のキュウ'; break;
+function getPron(c) {
+  let ret;
+  switch (c.toLowerCase()) {
+    case '0': ret='ゼロ';  ;break;
+    case '1': ret='イチ';  ;break;
+    case '2': ret='ニ';    ;break;
+    case '3': ret='サン';  ;break;
+    case '4': ret='ヨン';  ;break;
+    case '5': ret='ゴ';    ;break;
+    case '6': ret='ロク';  ;break;
+    case '7': ret='ナナ';  ;break;
+    case '8': ret='ハチ';  ;break;
+    case '9': ret='キュウ' ;break;
 
-    case 'a': ret = '小文字のエー'; break;
-    case 'b': ret = '小文字のビー'; break;
-    case 'c': ret = '小文字のシー'; break;
-    case 'd': ret = '小文字のデー'; break;
-    case 'e': ret = '小文字のイー'; break;
-    case 'f': ret = '小文字のエフ'; break;
-    case 'g': ret = '小文字のジー'; break;
-    case 'h': ret = '小文字のエイチ'; break;
-    case 'i': ret = '小文字のアイ'; break;
-    case 'j': ret = '小文字のジェイ'; break;
-    case 'k': ret = '小文字のケー'; break;
-    case 'l': ret = '小文字のエル'; break;
-    case 'm': ret = '小文字のエム'; break;
-    case 'n': ret = '小文字のエヌ'; break;
-    case 'o': ret = '小文字のオー'; break;
-    case 'p': ret = '小文字のピー'; break;
-    case 'q': ret = '小文字のキュー'; break;
-    case 'r': ret = '小文字のアール'; break;
-    case 's': ret = '小文字のエス'; break;
-    case 't': ret = '小文字のテー'; break;
-    case 'u': ret = '小文字のユー'; break;
-    case 'v': ret = '小文字のブイ'; break;
-    case 'w': ret = '小文字のダブリュー'; break;
-    case 'x': ret = '小文字のエックス'; break;
-    case 'y': ret = '小文字のワイ'; break;
-    case 'z': ret = '小文字のゼット'; break;
+    case 'a': ret = 'エー'; break;
+    case 'b': ret = 'ビー'; break;
+    case 'c': ret = 'シー'; break;
+    case 'd': ret = 'デー'; break;
+    case 'e': ret = 'イー'; break;
+    case 'f': ret = 'エフ'; break;
+    case 'g': ret = 'ジー'; break;
+    case 'h': ret = 'エイチ'; break;
+    case 'i': ret = 'アイ'; break;
+    case 'j': ret = 'ジェイ'; break;
+    case 'k': ret = 'ケー'; break;
+    case 'l': ret = 'エル'; break;
+    case 'm': ret = 'エム'; break;
+    case 'n': ret = 'エヌ'; break;
+    case 'o': ret = 'オー'; break;
+    case 'p': ret = 'ピー'; break;
+    case 'q': ret = 'キュー'; break;
+    case 'r': ret = 'アール'; break;
+    case 's': ret = 'エス'; break;
+    case 't': ret = 'テー'; break;
+    case 'u': ret = 'ユー'; break;
+    case 'v': ret = 'ブイ'; break;
+    case 'w': ret = 'ダブリュー'; break;
+    case 'x': ret = 'エックス'; break;
+    case 'y': ret = 'ワイ'; break;
+    case 'z': ret = 'ゼット'; break;
 
-    case 'A': ret = '大文字のエー'; break;
-    case 'B': ret = '大文字のビー'; break;
-    case 'C': ret = '大文字のシー'; break;
-    case 'D': ret = '大文字のデー'; break;
-    case 'E': ret = '大文字のイー'; break;
-    case 'F': ret = '大文字のエフ'; break;
-    case 'G': ret = '大文字のジー'; break;
-    case 'H': ret = '大文字のエイチ'; break;
-    case 'I': ret = '大文字のアイ'; break;
-    case 'J': ret = '大文字のジェイ'; break;
-    case 'K': ret = '大文字のケー'; break;
-    case 'L': ret = '大文字のエル'; break;
-    case 'M': ret = '大文字のエム'; break;
-    case 'N': ret = '大文字のエヌ'; break;
-    case 'O': ret = '大文字のオー'; break;
-    case 'P': ret = '大文字のピー'; break;
-    case 'Q': ret = '大文字のキュー'; break;
-    case 'R': ret = '大文字のアール'; break;
-    case 'S': ret = '大文字のエス'; break;
-    case 'T': ret = '大文字のテー'; break;
-    case 'U': ret = '大文字のユー'; break;
-    case 'V': ret = '大文字のブイ'; break;
-    case 'W': ret = '大文字のダブリュー'; break;
-    case 'X': ret = '大文字のエックス'; break;
-    case 'Y': ret = '大文字のワイ'; break;
-    case 'Z': ret = '大文字のゼット'; break;
-
+    case ' ': ret='スペース'; break;
+    case '!': ret='感嘆符、エクスクラメーションマーク、ビックリマーク'; break;
+    case '"': ret='引用符、クォーテーションマーク'; break;
+    case '#': ret='ナンバーサイン、シャープ'; break;
+    case '$': ret='ダラーサイン、ドル'; break;
+    case '%': ret='パーセント'; break;
+    case '&': ret='アンパサンド'; break;
+    case '\'': ret='アポストロフィ'; break;
+    case '(': ret='左カッコ'; break;
+    case ')': ret='右カッコ'; break;
+    case '*': ret='アスタリスク'; break;
+    case '+': ret='プラス'; break;
+    case ',': ret='カンマ'; break;
+    case '-': ret='ハイフン、マイナス'; break;
+    case '.': ret='ドット、ピリオド'; break;
+    case '/': ret='スラッシュ'; break;
+    case ':': ret='コロン'; break;
+    case ';': ret='セミコロン'; break;
+    case '<': ret='小なり記号'; break;
+    case '=': ret='イコール'; break;
+    case '>': ret='大なり記号'; break;
+    case '?': ret='はてな、クエスチョンマーク'; break;
+    case '@': ret='アットマーク'; break;
+    case '[': ret='左カギカッコ'; break;
+    case '\\': ret='バックスラッシュ'; break;
+    case ']': ret='右カギカッコ'; break;
+    case '^': ret='ハット'; break;
+    case '_': ret='アンダースコア'; break;
+    case '`': ret='バッククオート、バックチック'; break;
+    case '{': ret='左中カッコ'; break;
+    case '|': ret='バーティカルバー'; break;
+    case '}': ret='右中カッコ'; break;
+    case '~': ret='チルダ、にょろ'; break;    
   }
-
   return ret;
+}
+function getTusta2(c) {
+  let ret = {
+    pron: getPron(c),
+    isNumber: false,
+    isAlpha: false,
+    isCapital: false,
+  };
+  
+  if(Number.isInteger(Number(c))) {
+    ret.isNumber=true;
+  } else if( /^[a-z]*$/.test(c) ) {
+    ret.isAlpha=true;
+    ret.isCapital=false;
+  } else if( /^[A-Z]*$/.test(c) ) {
+    ret.isAlpha=true;
+    ret.isCapital=true;
+  }
+  return ret;
+}
+function getTusta(c) {
+  let ret = getTusta2(c);
+  let str = '';
+  if(ret.isNumber) {
+    str += '数字の';
+  } else if(ret.isAlpha) {
+    if(ret.isCapital) {
+      str += '大文字の';
+    } else {
+      str += '小文字の';
+    }
+  } 
+  str += ret.pron;
+  return str;
 }
 </script>
 
@@ -110,3 +149,8 @@ li {
   list-style: none;
 }
 </style>
+
+
+
+
+
